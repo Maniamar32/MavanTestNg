67ba4f1379e359a89baf47bc1623c2f54da52f85
mania <mania@DESKTOP-3B7KTDM>
2022-06-22 11:53:10 +0530

    third commit updated

--- RegularExpression/src/Dateprogram.java
+++ RegularExpression/src/Dateprogram.java
@@ -6,6 +6,9 @@ public class Dateprogram {
         
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter year");
+		System.out.println("github");
+
+
 		int y = sc.nextInt();
 		if((y%4==0)&&(y%100 !=0)||(y%400==0))
 		{
